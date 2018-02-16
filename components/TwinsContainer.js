fd5aa638fe3af56cd8ce1bebacff5d3617ea1475
Mohammad Rajabifard <mo.rajbi@gmail.com>
2018-02-17 00:04:03 +0330

    feat: add TwinsContainer

---
+++ components/TwinsContainer.js
@@ -0,0 +1,103 @@
+import styled, { css } from 'styled-components'
+
+import rem from '../utils/rem'
+import { mobile } from '../utils/media'
+
+const centerColumn = 1140
+const sidePadding = 25
+
+const TwinsContainer = ({
+  fullWidthOnMobile,
+  startWidth,
+  endWidth,
+  renderStart = () => {},
+  renderEnd = () => {},
+}) => {
+  const [first, second] = calcWidths(startWidth, endWidth, centerColumn)
+  const halfDiff = (first - second) / 2
+
+  return (
+    <Wrapper fullWidthOnMobile={fullWidthOnMobile}>
+      <Twin halfDiff={halfDiff} justifyContent="flex-end">
+        {renderStart(contentCreator('left', first))}
+      </Twin>
+      <Twin
+        halfDiff={halfDiff}
+        width={second}
+        justifyContent="flex-start"
+        isEnd={true}
+      >
+        {renderEnd(contentCreator('right', second, fullWidthOnMobile))}
+      </Twin>
+    </Wrapper>
+  )
+}
+
+export default TwinsContainer
+
+const contentCreator = (paddingPosition, width, fullWidthOnMobile) => props => (
+  <TwinContent
+    paddingPosition={paddingPosition}
+    width={width}
+    fullWidthOnMobile={fullWidthOnMobile}
+    {...props}
+  />
+)
+
+const calcWidths = (first, second, sum) => {
+  if (first) {
+    return [first, sum - first]
+  } else if (second) {
+    return [sum - second, second]
+  }
+
+  return [sum / 2, sum / 2] // default
+}
+
+const Wrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  width: 100%;
+
+  ${mobile(css`
+    flex-direction: ${p => (p.fullWidthOnMobile ? 'column' : 'row')};
+  `)};
+`
+
+const Twin = styled.div`
+  flex-grow: 1;
+  flex-basis: auto;
+  width: calc(50% ${p => (p.isEnd ? `-` : `+`)} ${p => rem(p.halfDiff)});
+  position: relative;
+  display: flex;
+  justify-content: ${p => p.justifyContent};
+
+  @media (max-width: ${rem(centerColumn)}) {
+    flex: 1 1 auto;
+    width: auto;
+  }
+`
+
+export const TwinContent = styled.div`
+  width: ${p => (p.width ? rem(p.width) : 'auto')};
+
+  @media (max-width: ${rem(centerColumn + sidePadding * 2)}) {
+    width: auto;
+    box-sizing: content-box;
+
+    ${p =>
+      p.paddingPosition === 'left'
+        ? css`
+            padding-left: ${rem(sidePadding)};
+          `
+        : p.paddingPosition === 'right'
+          ? css`
+              padding-right: ${rem(sidePadding)};
+            `
+          : null};
+  }
+
+  ${mobile(css`
+    width: ${p => (p.fullWidthOnMobile ? '100%' : 'auto')};
+  `)};
+`
