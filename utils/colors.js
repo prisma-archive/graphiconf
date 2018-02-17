0cb8b042de2d4cd7df7c78aba2b6fa2a02b4e06a
Mohammad Rajabifard <mo.rajbi@gmail.com>
2018-02-17 13:30:37 +0330

    feat: add new colors

--- utils/colors.js
+++ utils/colors.js
@@ -4,4 +4,7 @@ export const textDarkGrey = '#1F2228'
 export const textGrey = '#757575'
 export const textLightGrey = '#9B9B9B'
 export const bgLightGrey = '#f1f1f1'
+
 export const bgLightPink = '#FEEEF4'
+export const specialPink = '#DB3F74'
+export const muteBlue = '#ECEEF1'
