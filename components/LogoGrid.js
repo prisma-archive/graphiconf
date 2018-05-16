d7ced7b639394fd36dc79f3eed3437916c61682d
Mohammad Rajabifard <mo.rajbi@gmail.com>
2018-05-16 16:12:44 +0430

    New logo grid with more height width support

--- components/LogoGrid.js
+++ components/LogoGrid.js
@@ -7,25 +7,42 @@ export const Grid = styled.div`
   display: flex;
   flex-wrap: wrap;
   justify-content: center;
+  margin-bottom: 60px;
+  align-items: center;
 `
 
 export const GridItem = styled.div`
   flex: 0 1 auto;
-  height: ${p => (p.small ? rem(70) : rem(140))};
-  line-height: ${p => (p.small ? rem(70) : rem(140))};
+  height: ${p =>
+    p.height ? rem(p.height) : p.tiny ? rem(40) : p.small ? rem(70) : rem(90)};
+  line-height: ${p =>
+    p.height ? rem(p.height) : p.tiny ? rem(40) : p.small ? rem(70) : rem(90)};
+  max-height: ${p => p.maxHeight};
 
   vertical-align: middle;
   text-align: center;
-  margin: 0 10px;
+  margin: 0 ${p => (p.tiny ? 10 : p.small ? 35 : 45)}px;
+  margin-bottom: ${p => (p.tiny ? 30 : p.small ? 60 : 90)}px;
+
+  img {
+    height: 100%;
+    object-fit: contain;
+    object-position: center;
+  }
 
   ${mobile(css`
-    height: ${rem(120)};
-    line-height: ${rem(120)};
+    height: ${p =>
+      p.heightOnMobile
+        ? rem(p.heightOnMobile)
+        : p.tiny ? rem(45) : p.small ? rem(55) : rem(60)};
+    line-height: ${p =>
+      p.heightOnMobile
+        ? rem(p.heightOnMobile)
+        : p.tiny ? rem(45) : p.small ? rem(55) : rem(60)};
   `)};
 `
 
 export const LogoImage = styled.img`
-  max-width: 90%;
   display: inline-block;
   vertical-align: middle;
 
